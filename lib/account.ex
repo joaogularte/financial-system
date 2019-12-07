661e5b057f993b3525f273f231f2e7e86aa15cb6
joaogularte <jov96@hotmail.com>
2019-12-07 15:05:04 -0300

    refactor: Removido função create

--- lib/account.ex
+++ lib/account.ex
@@ -1,21 +1,7 @@
 defmodule Account do
   @moduledoc """
-
+  Define main fields to account entity
   """
   @enforce_keys [:name, :email]
   defstruct name: "", email: "", currency: "BR", amount: 0
-  
-  @doc """
-  
-  """
-  def create(name, email, currency \\ "BR", amount \\ 0) do
-    if byte_size(name) > 0 &&
-         byte_size(email) > 0 &&
-         byte_size(currency) > 0 &&
-         is_number(amount) do
-      {:ok, %Account{name: name, email: email, currency: currency, amount: 0}}
-    else
-      raise ArgumentError, message: "the argument value is invalid"
-    end
-  end
 end
