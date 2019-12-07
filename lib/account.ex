d9b46d4aaf85abb54784b753b868557d62edc408
joaogularte <jov96@hotmail.com>
2019-12-07 15:06:55 -0300

    docs: alterado @moduledoc

--- lib/account.ex
+++ lib/account.ex
@@ -1,6 +1,6 @@
 defmodule Account do
   @moduledoc """
-  Define main fields to account entity
+  Dcclare main fields to account entity
   """
   @enforce_keys [:name, :email]
   defstruct name: "", email: "", currency: "BR", amount: 0
