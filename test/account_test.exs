5dc2829c66213a9010df71c977cd5756696eb13d
joaogularte <jov96@hotmail.com>
2019-12-07 15:08:00 -0300

    test: deletado arquivo do account module

--- test/account_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule AccountTest do
-  use ExUnit.Case, async: true
-
-  doctest Account
-
-  test "create a account" do
-    assert Account.create("Joao Vitor Gularte", "joao@gmail.com", "BR", 12)
-  end 
-end
