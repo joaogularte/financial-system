0476c31d6e81b855a4f2b9b18960d998aa0019f7
joaogularte <jov96@hotmail.com>
2019-12-07 16:09:46 -0300

    fix: currency padrão

--- lib/account.ex
+++ lib/account.ex
@@ -3,5 +3,5 @@ defmodule Account do
   Declare main fields to account entity
   """
   @enforce_keys [:name, :email]
-  defstruct name: "", email: "", currency: "BR", amount: 0
+  defstruct name: "", email: "", currency: "BRL", amount: 0
 end
