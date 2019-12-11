cfe987138837ebb1d56e3ec88d5ba3b60c9de65e
joaogularte <jov96@hotmail.com>
2019-12-11 01:12:30 -0300

    feat: adicionado metodo list que busca as moedas em compliance com ISO4217 e seu teste.

--- lib/currency.ex
+++ lib/currency.ex
@@ -3,7 +3,30 @@ defmodule Currency do
 
   """
   use Agent
+  use Tesla
   plug(Tesla.Middleware.BaseUrl, "http://www.apilayer.net/api")
-  plug(Tesla.Middleware.JSON)
+  
   @access_key "272f531700cf18dcafcc40fd74b3559e"
+
+  @doc """
+  Get list of currencies in complience with ISO 4217 through Currencylayer API
+  ## Examples 
+      Currency.list()
+  """
+  @spec list() :: map()
+  def list() do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+
+    case do_list() do
+        :ok -> Agent.get(__MODULE__, fn state -> state end)
+        {:error} -> raise "Unable to get currency list from the server"
+    end 
+  end
+  @spec do_list() :: map()
+  defp do_list() do
+    case get("list?access_key=#{@access_key}") do
+      {:ok, response} -> Agent.update(__MODULE__, fn state -> Poison.decode!(response.body)["currencies"] end)
+      _error -> {:error}
+    end
+  end
 end
--- test/currency_test.exs
+++ test/currency_test.exs
@@ -1,3 +1,8 @@
 defmodule CurrencyTest do
   use ExUnit.Case, async: true
+  doctest Currency
+
+  test "Get currency list in complience with ISO 4217 from server" do
+      assert Currency.currency_list()
+  end
 end
