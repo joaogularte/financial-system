91bca25e03a68ccdd43c119cf5a2ad6482d16d92
joaogularte <joaogularte@u149326.unisc.br>
2019-12-12 15:19:34 -0300

    docs: Adicionado ExDocs

--- mix.exs
+++ mix.exs
@@ -16,7 +16,14 @@ defmodule FinancialSystem.MixProject do
         "coveralls.detail": :test,
         "coveralls.post": :test,
         "coveralls.html": :test
-      ]
+      ],
+
+      # ExDocs
+      name: "Financial System",
+      source_url: "https://github.com/joaogularte/financial-system",
+      homepage_url: "https://github.com/joaogularte/financial-system",
+      docs: [main: "FinancialSystem",
+            extras: ["README.md"]]
     ]
   end
 
@@ -33,7 +40,8 @@ defmodule FinancialSystem.MixProject do
       {:excoveralls, "~> 0.10", only: :test},
       {:tesla, "~> 1.3.0"},
       {:poison, "~> 3.1"},
-      {:decimal, "~> 1.0"}
+      {:decimal, "~> 1.0"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
